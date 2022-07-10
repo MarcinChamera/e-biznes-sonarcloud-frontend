28f349fa6b4a184bfc755cb281743090b1d7d74e
Marcin Chamera <mchamera237@gmail.com>
2022-07-10 21:25:55 +0200

    Removed unnecessary comments

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -3,22 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "http://localhost:1323",
 
-    // headers: {
-    //     "Content-Type": "application/json",
-    // },
-    // transformRequest: [
-    //     data => {
-    //       return JSON.stringify(data);
-    //     },
-    //   ],
-    //   transformResponse: [
-    //     data => {
-    //       return JSON.parse(data);
-    //     },
-    //   ],
     headers: {
-        // "Access-Control-Allow-Origin": "http://localhost:1323",
         "Content-Type": "application/json",
-        // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
       }
 })
